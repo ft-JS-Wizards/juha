f110b8928f13e95e0b54b01787826f73c3587901
conteng <contemplation.person@gmail.com>
2023-11-09 00:32:54 +0900

    refactor: :art: 중복코드 제거

--- stack_and_queue/programmers_42587.cpp
+++ stack_and_queue/programmers_42587.cpp
@@ -4,26 +4,28 @@
 
 using namespace std;
 
-int solution(vector<int> pr, int lo) {
+int solution(vector<int> pr, int lo)
+{
     int max = *max_element(pr.begin(), pr.end());
     int cnt = 0;
 
-    while(!pr.empty()) {
-        if (pr[0] == max) {
+    while (!pr.empty())
+    {
+        if (pr[0] == max)
+        {
             cnt++;
             if (lo == 0)
                 return cnt;
             pr.erase(pr.begin());
-            lo--;
-            if (lo < 0)
-                lo = pr.size() - 1;
             max = *max_element(pr.begin(), pr.end());
-        } else {
+        }
+        else
+        {
             pr.push_back(pr[0]);
             pr.erase(pr.begin());
-            lo--;
-            if (lo < 0)
-                lo = pr.size() - 1;
         }
+        lo--;
+        if (lo < 0)
+            lo = pr.size() - 1;
     }
 }
