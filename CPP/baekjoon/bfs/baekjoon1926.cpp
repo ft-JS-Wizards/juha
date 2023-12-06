981f628b17a9788eba1cb9c239f2073a527f1247
juha <contemplation.person@gmail.com>
2023-12-07 01:45:42 +0900

    feat: queue를 사용한 bfs공부, 조건문에 조금 더 주의를 기울일 것,

--- CPP/baekjoon/bfs/baekjoon1926.cpp
+++ CPP/baekjoon/bfs/baekjoon1926.cpp
@@ -18,7 +18,7 @@ using namespace std;
 출력
 첫째 줄에는 그림의 개수, 둘째 줄에는 그 중 가장 넓은 그림의 넓이를 출력하여라
 단, 그림이 하나도 없는 경우에는 가장 넓은 그림의 넓이는 0이다.*/
-void print_v(vector<vector<int> > r_v)
+void print_v(vector<vector<int>> r_v)
 {
     cout << "------ map ------" << endl;
     for (int i = 0; i < r_v.size(); i++)
@@ -38,23 +38,49 @@ void print_v(vector<vector<int> > r_v)
 #define WEST -1
 #define SOUTH 1
 
-void init_input(vector<vector<int > >& r_v, vector<int>& c_v, int& row, int& column, int tmp)
+void init_input(vector<vector<int>> &r_v, vector<int> &c_v, int &row, int &column, int tmp)
 {
     cin >> row >> column;
 
     r_v.reserve(row);
     c_v.reserve(column);
-    
+
     for (int i = 0; i < row; i++)
     {
         c_v.clear();
-        for (int j = 0;j < column; j++)
+        for (int j = 0; j < column; j++)
         {
             cin >> tmp;
             c_v.push_back(tmp);
         }
         r_v.push_back(c_v);
     }
+}
+
+void bfs(vector<vector<int>> &r_v, queue<pair<int, int>> &q, int &tmp)
+{
+    tmp++;
+    if (q.front().first + SOUTH < r_v.size() && r_v[q.front().first + SOUTH][q.front().second])
+    {
+        r_v[q.front().first + SOUTH][q.front().second] = VISIT;
+        q.push(make_pair(q.front().first + SOUTH, q.front().second));
+    }
+    if (q.front().second + EAST < r_v.front().size() && r_v[q.front().first][q.front().second + EAST])
+    {
+        r_v[q.front().first][q.front().second + EAST] = VISIT;
+        q.push(make_pair(q.front().first, q.front().second + EAST));
+    }
+    if (q.front().first > 0 && r_v[q.front().first + NORTH][q.front().second])
+    {
+        r_v[q.front().first + NORTH][q.front().second] = VISIT;
+        q.push(make_pair(q.front().first + NORTH, q.front().second));
+    }
+    if (q.front().second > 0 && r_v[q.front().first][q.front().second + WEST])
+    {
+        r_v[q.front().first][q.front().second + WEST] = VISIT;
+        q.push(make_pair(q.front().first, q.front().second + WEST));
+    }
+    q.pop();
 }
 
 int main()
@@ -67,48 +93,28 @@ int main()
     int paint_cnt = 0;
     int big_paint_area = 0;
     int cnt_one = 0;
-    queue<pair<int, int> > q;
-    vector<vector<int> > r_v;
+    queue<pair<int, int>> q;
+    vector<vector<int>> r_v;
     vector<int> c_v;
 
     init_input(r_v, c_v, row, column, tmp);
-    
+
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < column; j++)
         {
             if (r_v[i][j])
             {
+                // print_v(r_v);
                 r_v[i][j] = VISIT;
                 q.push(make_pair(i, j));
                 paint_cnt++;
                 tmp = 0;
                 while (!q.empty())
                 {
-                    tmp++;
-                    if (q.front().first + SOUTH < row && r_v[q.front().first + SOUTH][q.front().second])
-                    {
-                        r_v[q.front().first + SOUTH][q.front().second] = VISIT;
-                        q.push(make_pair(q.front().first + SOUTH, q.front().second));
-                    }
-                    if (q.front().second + EAST < column && r_v[q.front().first][q.front().second + EAST])
-                    {
-                        r_v[q.front().first][q.front().second + EAST] = VISIT;
-                        q.push(make_pair(q.front().first, q.front().second + EAST));
-                    }
-                    if (q.front().first + NORTH > -1 && r_v[q.front().first + NORTH][q.front().second])
-                    {
-                        r_v[q.front().first + NORTH][q.front().second] = VISIT;
-                        q.push(make_pair(q.front().first + NORTH, q.front().second));
-                    }
-                    if (q.front().second + WEST < column && r_v[q.front().first][q.front().second + WEST])
-                    {
-                        r_v[q.front().first][q.front().second + WEST] = VISIT;
-                        q.push(make_pair(q.front().first, q.front().second + WEST));
-                    }
-                    q.pop();
+                    bfs(r_v, q, tmp);
                 }
-                if (big_paint_area > tmp)
+                if (big_paint_area < tmp)
                 {
                     big_paint_area = tmp;
                 }
@@ -116,5 +122,6 @@ int main()
         }
     }
 
-    cout << big_paint_area << endl << paint_cnt << endl ;
+    cout << paint_cnt << endl
+         << big_paint_area << endl;
 }
