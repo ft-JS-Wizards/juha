61bc92f328e4845cf22b576331a9053b5d3146e0
juha <contemplation.person@gmail.com>
2024-02-16 19:08:50 +0900

    test

--- algorithm/JS/programmers120837.js
+++
@@ -1,9 +0,0 @@
-function solution(hp) {
-  const ant = [5, 3, 1];
-  let cnt = 0;
-  for (num of ant) {
-    cnt += ~~(hp / num);
-    hp = hp % num;
-  }
-  return cnt;
-}
--- algorithm/README.md
+++
@@ -1,2 +0,0 @@
-# JuHa
-cpp algorithm
